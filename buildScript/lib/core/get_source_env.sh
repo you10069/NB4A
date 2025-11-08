3f6619807d4c9ed410141bdf60b7113c2c1317c5
you10069 <104882329+you10069@users.noreply.github.com>
2025-11-08 12:55:22 +0800

    1.2.9

---
+++ buildScript/lib/core/get_source_env.sh
@@ -0,0 +1,8 @@
+if [ ! -z $ENV_NB4A ]; then
+  export COMMIT_SING_BOX_EXTRA="2f8e1bdb842ec2138c1699101a84950617c3cbb8"
+fi
+
+if [ ! -z $ENV_SING_BOX_EXTRA ]; then
+  source libs/get_source_env.sh
+  # export COMMIT_SING_BOX="91495e813068294aae506fdd769437c41dd8d3a3"
+fi
